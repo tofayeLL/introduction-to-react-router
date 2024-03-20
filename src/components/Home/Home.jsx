459a8eea289c578cd0a47c023bbff3b083b1cbca
tofayeLL <ahmedtofayel132@gmail.com>
2024-03-21 02:12:24 +0600

    learned and practice about to see path location or route location by useLocation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,12 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
     const navigation = useNavigation();
+
+    const location = useLocation();
+    console.log(location);
     return (
         <div>
             <Header></Header>
